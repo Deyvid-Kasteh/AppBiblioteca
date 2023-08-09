ee9dea1a11f22b96c01d5e069c0c7d17ef61a324
Deyvid Kasteh <kasteh21@gmail.com>
2023-08-09 15:21:11 -0300

    Loggin Implementation

---
+++ .history/firebaseConfig_20230809023413.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB7Qfd75GcWhLAJqaUTaUtedKbDDmN4WhU",
+  authDomain: "appbiblioteca-afa10.firebaseapp.com",
+  projectId: "appbiblioteca-afa10",
+  storageBucket: "appbiblioteca-afa10.appspot.com",
+  messagingSenderId: "222273696743",
+  appId: "1:222273696743:web:1d3db88f54d18c56784342",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
