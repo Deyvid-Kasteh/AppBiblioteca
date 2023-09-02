6f0784ad902f23cfbd5a084425a2d1877df44998
Deyvid Kasteh <kasteh21@gmail.com>
2023-09-02 19:13:01 -0300

    mesclagem 3

---
+++ .history/screens/Book_20230902145339.js
@@ -0,0 +1,242 @@
+import React, { useEffect, useState } from "react";
+import {
+  Button,
+  Image,
+  TouchableOpacity,
+  Pressable,
+  ScrollView,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
+import { buscaLivroPorId } from "../services/api";
+import { AntDesign } from "@expo/vector-icons";
+import { Ionicons } from "@expo/vector-icons";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import parse from "html-react-parser";
+
+export default function Book({ navigation: { goBack }, route }) {
+  const [livro, setLivro] = useState(null);
+  const [bookFav, setBookFav] = useState(false);
+  const [shoppingCart, setShoppingCart] = useState(false);
+  const book = route.params.idLivro;
+  const bookfavoriter = async () => {
+    if (bookFav) {
+
+    } else {
+
+    }
+
+}
+
+  const getData = async (idLivro) => {
+    try {
+      const jsonValue = await AsyncStorage.getItem("@user");
+      const data = JSON.parse(jsonValue);
+      console.log(
+        "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+      );
+      console.log(data.books);
+      const books = data.books;
+
+      let fav = books.some((boo) => boo.idLivro == book);
+      console.log(fav);
+      setBookFav(fav);
+    } catch (e) {
+      // error reading value
+    }
+  };
+
+  BuscaLivro = async (idLivro) => {
+    const response = await buscaLivroPorId(idLivro);
+    console.log(response.data);
+    setLivro(() => response.data);
+    getData();
+  };
+
+  useEffect(() => {
+    BuscaLivro(route.params.idLivro);
+  }, []);
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "flex-start",
+        alignItems: "center",
+        backgroundColor: "#F2E2C4",
+      }}
+    >
+      {livro ? (
+        <>
+          <View
+            style={{
+              flex: 0.95,
+              flexDirection: "column",
+              justifyContent: "center",
+              alignItems: "center",
+              margin: 10,
+              paddingBottom: 20,
+              backgroundColor: "#F2E2C4",
+            }}
+          >
+            <View
+              style={{
+                flex: 1,
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
+              <View
+                style={{
+                  flex: 1,
+                  // flexDirection: "row",
+                  justifyContent: "center",
+                  alignItems: "center",
+                }}
+              >
+                <Image
+                  style={{
+                    width: 240,
+                    height: 384,
+                    borderRadius: 10,
+                    // alignSelf: "center",
+                  }}
+                  source={{
+                    uri: `${livro.volumeInfo.imageLinks.thumbnail}`,
+                  }}
+                />
+              </View>
+              <View
+                style={{
+                  position: "absolute",
+                  right: 5,
+                  top: 10,
+                  justifyContent: "center",
+                  alignItems: "center",
+                }}
+              >
+                <TouchableOpacity
+                  style={{
+                    width: 48,
+                    height: 48,
+                    backgroundColor: "#D9B391",
+                    borderRadius: 50,
+                    justifyContent: "center",
+                    alignItems: "center",
+                  }}
+                  onPress={() => bookFav}
+                >
+                  {bookFav ? (
+                    <AntDesign name="heart" size={30} color="#A65A49" />
+                  ) : (
+                    <AntDesign name="hearto" size={30} color="#A65A49" />
+                  )}
+                </TouchableOpacity>
+              </View>
+
+              <Text
+                numberOfLines={2}
+                style={{
+                  fontSize: 30,
+                  color: "#2B3640",
+                }}
+              >
+                {livro.volumeInfo.title}
+                {/*                 {console.log(parse(livro.volumeInfo.description))}
+                 */}
+              </Text>
+
+              <View
+                style={{
+                  height: 164,
+                }}
+              >
+                <ScrollView>
+                  <Text
+                    // numberOfLines={8}
+                    style={{
+                      fontSize: 16,
+                      color: "#2B3640",
+                    }}
+                  >
+                    {livro.volumeInfo.description}
+                  </Text>
+                </ScrollView>
+              </View>
+            </View>
+
+            <View
+              style={{
+                flexDirection: "row",
+                marginTop: 20,
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
+              <TouchableOpacity
+                style={{
+                  width: 120,
+                  height: 52,
+                  backgroundColor: "#D9B391",
+                  borderRadius: 50,
+                  justifyContent: "center",
+                  alignItems: "center",
+                  marginRight: 12,
+                  borderWidth: 2,
+                  borderColor: "#A65A49",
+                }}
+              >
+                <Text
+                  style={{
+                    fontSize: 20,
+                    color: "#2d3741",
+                  }}
+                >
+                  Comprar
+                </Text>
+              </TouchableOpacity>
+
+              <TouchableOpacity
+                style={{
+                  width: 48,
+                  height: 48,
+                  backgroundColor: "#D9B391",
+                  borderRadius: 50,
+                  justifyContent: "center",
+                  alignItems: "center",
+                  position: "absolute",
+                  right: -52,
+                }}
+                onPress={() => setShoppingCart(!shoppingCart)}
+              >
+                {shoppingCart ? (
+                  <Ionicons name="cart-sharp" size={30} color="#A65A49" />
+                ) : (
+                  <Ionicons name="cart-outline" size={30} color="#A65A49" />
+                )}
+              </TouchableOpacity>
+            </View>
+          </View>
+        </>
+      ) : (
+        <>
+          <View
+            style={{
+              flex: 0.95,
+              flexDirection: "column",
+              justifyContent: "center",
+              alignItems: "center",
+              margin: 10,
+              paddingBottom: 20,
+              backgroundColor: "#F2E2C4",
+            }}
+          >
+            <Button title="Voltar" onPress={() => goBack()} />
+          </View>
+        </>
+      )}
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
