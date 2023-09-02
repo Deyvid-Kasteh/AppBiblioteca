7f62bae0ec27b93f719d005447d5edc42f583bfc
Deyvid Kasteh <kasteh21@gmail.com>
2023-09-02 19:16:26 -0300

    tentativa de resolução do problema de mesclagens

--- screens/Book.js
+++ screens/Book.js
@@ -21,21 +21,6 @@ export default function Book({ navigation: { goBack }, route }) {
   const [bookFav, setBookFav] = useState(false);
   const [shoppingCart, setShoppingCart] = useState(false);
   const book = route.params.idLivro;
-<<<<<<< HEAD
-
-=======
-  const bookfavoriter = async () => {
-    if (!bookFav) {
-      console.log("Favoritar");
-      console.log("");
-      console.log("");
-    } else {
-      console.log("Desfavoritar");
-      console.log("");
-      console.log("");
-    }
-  };
->>>>>>> 89cb81251d6f5ca7f975f2e99c04a953fe8d1ced
 
   const getData = async () => {
     try {
