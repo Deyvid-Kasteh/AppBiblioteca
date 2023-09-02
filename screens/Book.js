117287d394982e9b85b5233cc534f3895e91658b
Deyvid Kasteh <kasteh21@gmail.com>
2023-09-02 19:17:38 -0300

    mais um teste de mesclagem

--- screens/Book.js
+++ screens/Book.js
@@ -22,6 +22,18 @@ export default function Book({ navigation: { goBack }, route }) {
   const [shoppingCart, setShoppingCart] = useState(false);
   const book = route.params.idLivro;
 
+  const bookfavoriter = async () => {
+    if (!bookFav) {
+      console.log("Favoritar");
+      console.log("");
+      console.log("");
+    } else {
+      console.log("Desfavoritar");
+      console.log("");
+      console.log("");
+    }
+  };
+
   const getData = async () => {
     try {
       const jsonValue = await AsyncStorage.getItem("@user");
