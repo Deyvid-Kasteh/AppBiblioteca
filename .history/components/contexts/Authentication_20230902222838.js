3d471f9398285e30009e1646c451e163e91e5234
Deyvid Kasteh <kasteh21@gmail.com>
2023-09-03 01:18:17 -0300

    book favoriter done

---
+++ .history/components/contexts/Authentication_20230902222838.js
@@ -0,0 +1,246 @@
+import React, { createContext, useState } from "react";
+import {
+  GoogleSignin,
+  GoogleSigninButton,
+} from "@react-native-google-signin/google-signin";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { api, createSession } from "../../services/api";
+import { useNavigation } from "@react-navigation/native";
+
+export const AuthContext = createContext({});
+
+function AuthProvider({ children }) {
+  const navigation = useNavigation();
+
+  const [usuarioEstaLogado, setUsuarioEstaLogado] = useState(false);
+  const [usuario, setUsuario] = useState(null);
+
+  SignIn = async function (name, email, password) {
+    console.log("Começou INSCRIÇÃO");
+    const data = {
+      name,
+      email,
+      password,
+    };
+
+    try {
+      // const response = await createSession(email, password);
+      const response = await api.post("/users", data);
+      console.log(
+        "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+      );
+      console.log(response);
+      console.log(
+        "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+      );
+
+      console.log(response.data);
+      console.log(
+        "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+      );
+
+      console.log("vai pro navigate pra voltar");
+      navigation.navigate("Login");
+      return;
+    } catch (error) {
+      console.log("veio pro error");
+      console.error(error);
+    }
+  };
+  SignInGoogle = async function (name, email, password) {
+    console.log("Começou INSCRIÇÃO");
+    const data = {
+      name,
+      email,
+      password,
+    };
+    try {
+      const responseSignIn = await api.post("/users", data);
+      const responseCreateSession = await createSession(email, password);
+      console.log(responseCreateSession.data.user);
+      api.defaults.headers.authorization = `Bearer ${responseCreateSession.data.token}`;
+
+      console.log("vai pro navigate pra voltar");
+      navigation.goBack();
+      return;
+    } catch (error) {
+      console.log("veio pro error");
+      console.error(error);
+    }
+  };
+
+  Login = async function (email, password) {
+    console.log("Começou CONTEXT");
+
+    try {
+      const response = await createSession(email, password);
+      api.defaults.headers.authorization = `Bearer ${response.data.token}`;
+      console.log(response.data.token);
+      const responseUpdated = await api.get(`/Perfil/${response.data.user.id}`);
+
+      console.log("Começou STOREDATA");
+      const jsonValue = JSON.stringify(responseUpdated.data);
+      console.log(jsonValue);
+      await AsyncStorage.setItem("@user", jsonValue);
+      setUsuario(responseUpdated.data);
+      setUsuarioEstaLogado(true);
+      console.log("vai pro navigate pra voltar");
+      navigation.navigate("HomeStart");
+      return;
+    } catch (error) {
+      console.log("veio pro error");
+      console.error(error);
+    }
+  };
+
+  Favoriter = async function (id, idLivro, imgLivro, ttlLivro) {
+    console.log("Começou FAVORITER");
+    try {
+      const response = await api.patch(
+        `/Perfil/${id}/addBookToFavorites/${idLivro}`,
+        { idLivro, imgLivro, ttlLivro }
+      );
+      console.log("FOI");
+      data = response.data;
+      await AsyncStorage.setItem("@user", data);
+      setUsuario(data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  Unfavorater = async function (id, idLivro, imgLivro, ttlLivro) {
+
+    console.log("Começou UNFAVORITER");
+    try {
+
+    } catch (error) {
+      
+
+    }
+
+  }
+
+  SignInProcess = async () => {
+    GoogleSignin.configure();
+    try {
+      console.log("Começou SIGN-IN");
+      await GoogleSignin.hasPlayServices();
+      const signInResult = await GoogleSignin.signIn();
+      console.log(signInResult);
+      console.log("Começou STOREDATA");
+      const jsonValue = JSON.stringify(signInResult.user);
+      console.log(jsonValue);
+      await AsyncStorage.setItem("@user", jsonValue);
+      setUsuarioEstaLogado(true);
+      setUsuario(signInResult);
+      console.log(signInResult.user.photo);
+      navigation.goBack();
+    } catch (error) {
+      if (error.code === statusCodes.SIGN_IN_CANCELLED) {
+        console.error("user cancelled the login flow");
+      } else if (error.code === statusCodes.IN_PROGRESS) {
+        console.error("operation (e.g. sign in) is in progress already");
+      } else if (error.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE) {
+        console.error("play services not available or outdated");
+      } else {
+        console.error("some other error happened");
+        console.error(error);
+      }
+    }
+  };
+
+  SignInGoogleProcess = async () => {
+    GoogleSignin.configure();
+    try {
+      await GoogleSignin.hasPlayServices();
+      const signInResult = await GoogleSignin.signIn();
+      const data = {
+        name: signInResult.user.name,
+        email: signInResult.user.email,
+        password: signInResult.user.id,
+      };
+      const pic = signInResult.user.photo;
+      try {
+        const responseSignInMongoDb = await api.post("/users", data);
+        if (responseSignInMongoDb !== null) {
+          try {
+            const responseCreateSession = await createSession(
+              signInResult.user.email,
+              signInResult.user.id
+            );
+            api.defaults.headers.authorization = `Bearer ${responseCreateSession.data.token}`;
+            const dataPic = {
+              pic,
+            };
+            await api.patch(
+              `/Perfil/${responseCreateSession.data.user.id}/pic`,
+              dataPic
+            );
+            const responseUpdated = await api.get(
+              `/Perfil/${responseCreateSession.data.user.id}`
+            );
+            const jsonValue = JSON.stringify(responseUpdated.data);
+            await AsyncStorage.setItem("@user", jsonValue);
+            setUsuario(() => responseUpdated.data);
+            setUsuarioEstaLogado(true);
+          } catch (error) {
+            console.error(error);
+          }
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    } catch (error) {
+      if (error.code === statusCodes.SIGN_IN_CANCELLED) {
+        console.error("user cancelled the login flow");
+      } else if (error.code === statusCodes.IN_PROGRESS) {
+        console.error("operation (e.g. sign in) is in progress already");
+      } else if (error.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE) {
+        console.error("play services not available or outdated");
+      } else {
+        console.error("some other error happened");
+        console.error(error);
+      }
+    }
+  };
+
+  signOutProcess = async () => {
+    GoogleSignin.configure();
+
+    try {
+      await GoogleSignin.signOut();
+      // setUserInfo(null); // Remember to remove the user from your app's state as well
+      console.log("SAIU");
+      console.log("SAIU");
+      await AsyncStorage.removeItem("@user");
+      console.log("SAIU");
+      console.log("SAIU");
+      setUsuarioEstaLogado(false);
+      setUsuario(null);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <AuthContext.Provider
+      value={{
+        nome: "Deyvid Kasteh",
+        SignInProcess,
+        signOutProcess,
+        usuarioEstaLogado,
+        usuario,
+        Login,
+        SignIn,
+        SignInGoogleProcess,
+        SignInGoogle,
+        Favoriter,
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+}
+
+export default AuthProvider;
---
+++ .history/screens/Book_20230902211754.js
@@ -0,0 +1,283 @@
+import React, { useEffect, useState, useContext } from "react";
+import {
+  Button,
+  Image,
+  TouchableOpacity,
+  Pressable,
+  ScrollView,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
+import { api } from "../services/api";
+import { buscaLivroPorId } from "../services/api";
+import { AntDesign } from "@expo/vector-icons";
+import { Ionicons } from "@expo/vector-icons";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { AuthContext } from "../components/contexts/Authentication";
+
+import parse from "html-react-parser";
+import sanitizeHtml from "sanitize-html";
+
+export default function Book({ navigation: { goBack }, route }) {
+  const { usuarioEstaLogado, usuario } = useContext(AuthContext);
+
+  const [livro, setLivro] = useState(null);
+  const [bookFav, setBookFav] = useState(false);
+  const [shoppingCart, setShoppingCart] = useState(false);
+  const book = route.params.idLivro;
+
+  const bookfavoriter = async () => {
+    try {
+      if (usuarioEstaLogado) {
+        if (!bookFav) {
+          console.log("Favoritar");
+          const response = await api.patch(
+            `/Perfil/${idUsuario}/addBookToFavorites/${idLivro}`,
+            { idLivro, imgLivro, ttlLivro }
+          );
+
+          console.log("");
+          console.log("");
+        } else {
+          console.log("Desfavoritar");
+          console.log("");
+          console.log("");
+        }
+      } else {
+      }
+    } catch (error) {}
+  };
+
+  const getData = async () => {
+    try {
+      const jsonValue = await AsyncStorage.getItem("@user");
+      const data = JSON.parse(jsonValue);
+      // console.log(
+      //   "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+      // );
+
+      // console.log(data);
+
+      // console.log(
+      //   "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+      // );
+      // console.log(data._id);
+
+      console.log(
+        "======================================"
+      );
+
+      // console.log(usuario);
+
+      // console.log("======================================");
+
+      // console.log(
+      //   "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+      // );
+
+      const books = data.books;
+      let fav = books.some((boo) => boo.idLivro == book);
+      // console.log(fav);
+      setBookFav(fav);
+    } catch (e) {
+      // error reading value
+    }
+  };
+
+  BuscaLivro = async (idLivro) => {
+    const response = await buscaLivroPorId(idLivro);
+    console.log(response.data);
+    setLivro(() => response.data);
+    getData();
+  };
+
+  useEffect(() => {
+    BuscaLivro(route.params.idLivro);
+  }, []);
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "flex-start",
+        alignItems: "center",
+        backgroundColor: "#F2E2C4",
+      }}
+    >
+      {livro ? (
+        <>
+          <View
+            style={{
+              flex: 0.95,
+              flexDirection: "column",
+              justifyContent: "center",
+              alignItems: "center",
+              margin: 10,
+              paddingBottom: 20,
+              backgroundColor: "#F2E2C4",
+            }}
+          >
+            <View
+              style={{
+                flex: 1,
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
+              <View
+                style={{
+                  flex: 1,
+                  // flexDirection: "row",
+                  justifyContent: "center",
+                  alignItems: "center",
+                }}
+              >
+                <Image
+                  style={{
+                    width: 240,
+                    height: 384,
+                    borderRadius: 10,
+                    // alignSelf: "center",
+                  }}
+                  source={{
+                    uri: `${livro.volumeInfo.imageLinks.thumbnail}`,
+                  }}
+                />
+              </View>
+              <View
+                style={{
+                  position: "absolute",
+                  right: 5,
+                  top: 10,
+                  justifyContent: "center",
+                  alignItems: "center",
+                }}
+              >
+                <TouchableOpacity
+                  style={{
+                    width: 48,
+                    height: 48,
+                    backgroundColor: "#D9B391",
+                    borderRadius: 50,
+                    justifyContent: "center",
+                    alignItems: "center",
+                  }}
+                  onPress={() => bookfavoriter()}
+                >
+                  {bookFav ? (
+                    <AntDesign name="heart" size={30} color="#A65A49" />
+                  ) : (
+                    <AntDesign name="hearto" size={30} color="#A65A49" />
+                  )}
+                </TouchableOpacity>
+              </View>
+
+              <Text
+                numberOfLines={2}
+                style={{
+                  fontSize: 30,
+                  color: "#2B3640",
+                }}
+              >
+                {livro.volumeInfo.title}
+              </Text>
+
+              <View
+                style={{
+                  height: 164,
+                }}
+              >
+                <ScrollView>
+                  <Text
+                    // numberOfLines={8}
+                    style={{
+                      fontSize: 16,
+                      color: "#2B3640",
+                    }}
+                  >
+                    {/* {livro.volumeInfo.description} */}
+                    {livro
+                      ? sanitizeHtml(livro.volumeInfo.description, {
+                          allowedTags: [],
+                        })
+                      : livro.volumeInfo.description}
+                  </Text>
+                </ScrollView>
+              </View>
+            </View>
+
+            <View
+              style={{
+                flexDirection: "row",
+                marginTop: 20,
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
+              <TouchableOpacity
+                style={{
+                  width: 120,
+                  height: 52,
+                  backgroundColor: "#D9B391",
+                  borderRadius: 50,
+                  justifyContent: "center",
+                  alignItems: "center",
+                  marginRight: 12,
+                  borderWidth: 2,
+                  borderColor: "#A65A49",
+                }}
+              >
+                <Text
+                  style={{
+                    fontSize: 20,
+                    color: "#2d3741",
+                  }}
+                >
+                  Comprar
+                </Text>
+              </TouchableOpacity>
+
+              <TouchableOpacity
+                style={{
+                  width: 48,
+                  height: 48,
+                  backgroundColor: "#D9B391",
+                  borderRadius: 50,
+                  justifyContent: "center",
+                  alignItems: "center",
+                  position: "absolute",
+                  right: -52,
+                }}
+                onPress={() => setShoppingCart(!shoppingCart)}
+              >
+                {shoppingCart ? (
+                  <Ionicons name="cart-sharp" size={30} color="#A65A49" />
+                ) : (
+                  <Ionicons name="cart-outline" size={30} color="#A65A49" />
+                )}
+              </TouchableOpacity>
+            </View>
+          </View>
+        </>
+      ) : (
+        <>
+          <View
+            style={{
+              flex: 0.95,
+              flexDirection: "column",
+              justifyContent: "center",
+              alignItems: "center",
+              margin: 10,
+              paddingBottom: 20,
+              backgroundColor: "#F2E2C4",
+            }}
+          >
+            <Button title="Voltar" onPress={() => goBack()} />
+          </View>
+        </>
+      )}
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
