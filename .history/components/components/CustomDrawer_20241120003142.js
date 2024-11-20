effee9532bde58077a77dc59841f69d3b55c1680
Deyvid Kasteh <kasteh21@gmail.com>
2024-11-20 00:38:24 -0300

    CustomDrawer improvements

---
+++ .history/components/components/CustomDrawer_20241120003142.js
@@ -0,0 +1,50 @@
+import { View, Text } from "react-native";
+import React, { useContext } from "react";
+import {
+  DrawerContentScrollView,
+  DrawerItemList,
+} from "@react-navigation/drawer";
+import { AuthContext } from "../contexts/Authentication";
+import COLOR
+import Avatar from "../../assets/Avatar/Avatar";
+
+export default function CustomDrawer(props) {
+  const { usuario } = useContext(AuthContext);
+  return (
+    <DrawerContentScrollView {...props}>
+      <View
+        style={{
+          alignSelf: "center",
+          marginBottom: 30,
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <View
+          style={{
+            backgroundColor: ,
+            marginBottom: 40,
+            // margin: 10,
+            padding: 8,
+            borderRadius: 100,
+          }}
+        >
+          <Avatar width={110} height={110} />
+        </View>
+        <Text
+          style={{
+            color: COLORS.caramel,
+            fontSize: 25,
+            fontWeight: "bold",
+            marginBottom: 40,
+          }}
+        >
+          {usuario?.name}
+        </Text>
+      </View>
+      <View>
+        <DrawerItemList {...props} />
+      </View>
+    </DrawerContentScrollView>
+  );
+}
---
+++ .history/components/components/CustomDrawer_20241120003341.js
@@ -0,0 +1,53 @@
+import { View, Text } from "react-native";
+import React, { useContext } from "react";
+import {
+  DrawerContentScrollView,
+  DrawerItemList,
+} from "@react-navigation/drawer";
+import { AuthContext } from "../contexts/Authentication";
+import COLORS
+
+import Avatar from "../../assets/Avatar/Avatar";
+
+export default function CustomDrawer(props) {
+  const { usuario } =
+    useContext(AuthContext);
+  return (
+    <DrawerContentScrollView {...props}>
+      <View
+        style={{
+          alignSelf: "center",
+          marginBottom: 30,
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <View
+          style={{
+            // backgroundColor: "#D9B391",
+            backgroundColor: "#BF7F5A",
+            marginBottom: 40,
+            // margin: 10,
+            padding: 8,
+            borderRadius: 100,
+          }}
+        >
+          <Avatar width={110} height={110} />
+        </View>
+        <Text
+          style={{
+            color: "#A65A49",
+            fontSize: 25,
+            fontWeight: "bold",
+            marginBottom: 40,
+          }}
+        >
+          {usuario?.name}
+        </Text>
+      </View>
+      <View>
+        <DrawerItemList {...props} />
+      </View>
+    </DrawerContentScrollView>
+  );
+}
